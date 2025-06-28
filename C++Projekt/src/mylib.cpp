512296c8d8b433648f8c6a544e11f3e34ad2ce9c
David Mertens <david_tim_luca.mertens@smail.th-koeln.de>
2025-06-29 00:12:29 +0200

    _

--- C++Projekt/src/mylib.cpp
+++
@@ -1,7 +0,0 @@
-﻿// mylib.cpp
-#define MYLIBRARY_EXPORTS
-#include "mylib.h"
-
-int add(int a, int b) {
-    return a + b;
-}
--- JuceDllFac/src/main.cpp
+++
@@ -1,8 +0,0 @@
-﻿#include <iostream>
-
-
-extern "C" __declspec(dllexport)
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
