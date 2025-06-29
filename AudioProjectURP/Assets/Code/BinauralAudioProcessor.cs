4766c2f9708d8234c322b5babcf707f089a018ca
David Mertens <david_tim_luca.mertens@smail.th-koeln.de>
2025-06-29 16:37:54 +0200

    fixed Klicking

--- AudioProjectURP/Assets/Code/BinauralAudioProcessor.cs
+++ AudioProjectURP/Assets/Code/BinauralAudioProcessor.cs
@@ -13,64 +13,58 @@ namespace Code
     [RequireComponent(typeof(AudioSource))]
     public class BinauralAudioProcessor : MonoBehaviour
     {
-        public Transform sourceObject;
         public Transform targetObject;
         public AudioFileLoader audioFileLoader;
-        public float earOffset = 0.1f; // Abstand der Ohren zur Mitte in Metern
+        
         public bool bypass = false;
         public bool useDirect = false;
         public bool usePrimaryReflections = false;
         public bool useSecondaryReflections = false;
         public bool useHigherOrderReflections = false;
+       
         public float Gain;
+        
         public LiveConvolutionReverb reverb;
+        
         public AudioRay DirectHit;
         public List<AudioRay> PrimaryReflections;
         public List<AudioRay> SecundaryReflections;
         public List<AudioRay> HigherOrderReflections;
-
-        public float delaySmoothFactor = 0;
-
-        public float[] _impulseResponseLeft;
+        
+        private float[] _impulseResponseLeft;
         private float[] _impulseResponseRight;
+        
+        private List<float[]> _previousImpulseResponsesLeft;
+        private List<float[]> _previousImpulseResponsesRight;
 
-        private float[] _delayBufferLeft;
-        private float[] _delayBufferRight;
         private float _timeSinceLastImpulse;
 
         private int _bufferLength;
-        private int _writeIndex;
         private int _sampleRate;
 
         private Vector3 _leftEar;
         private Vector3 _rightEar;
 
-        private float prevLeftDelaySamples = 0f;
-        private float prevRightDelaySamples = 0f;
         private bool _isSetup;
-        private int _blockSize;
-        private IntPtr _convLeft, _convRight;
-        private ConvolutionReverbShap _reverb;
+        
         private float[] _overlapBufferLeft;
         private float[] _overlapBufferRight;
+        
+        private Complex[] _freqDomainIrLeft;
+        private Complex[] _freqDomainIrRight;
+        private readonly float earOffset = 0.1f; // Abstand der Ohren zur Mitte in Metern
 
         private void Awake()
         {
             _isSetup = false;
-            AudioSettings.GetDSPBufferSize(out _blockSize, out _);
-            _reverb = new ConvolutionReverbShap(2048, 1024);
         }
 
         private void Start()
         {
-            _writeIndex = 0;
             _sampleRate = AudioSettings.outputSampleRate;
-            _bufferLength = _sampleRate * 5;
-            _delayBufferLeft = new float[_bufferLength];
-            _delayBufferRight = new float[_bufferLength];
-            
+            _previousImpulseResponsesLeft = new List<float[]>();
+            _previousImpulseResponsesRight = new List<float[]>();
             _isSetup = true;
-            ConvolutionReverb.initReverb(_sampleRate, _blockSize, 2);
         }
 
         private void OnDestroy()
@@ -84,14 +78,11 @@ namespace Code
             _rightEar = targetObject.transform.position + targetObject.transform.right * earOffset;
 
             CreatePrimitiveImpulseresponse();
-            _timeSinceLastImpulse += Time.deltaTime;
         }
 
         public void CreatePrimitiveImpulseresponse()
         {
-            if (_timeSinceLastImpulse < 0.1) return;
-            _timeSinceLastImpulse = 0;
-            int irLength = 2024;
+            int irLength = 2024 * 2;
             // TOTO DAVID MARTIN KARG __ Diese Funktion sollte mit der HRTF Funktion ersetzt werden
             if (bypass || !_isSetup) return;
             _impulseResponseLeft = new float[irLength];
@@ -115,13 +106,13 @@ namespace Code
                 float maxEarDist = Vector3.Distance(_rightEar, _leftEar);
                 float binauralFactor = Mathf.Clamp((leftDistance - rightDistance) / (4 * maxEarDist), -1f, 1f);
                 float averageDistance = (leftDistance + rightDistance) / 2;
-                float DistanceAmplitude = 2 / averageDistance;
+                float distanceAmplitude = 2 / averageDistance;
 
                 if ((int)targetLeftDelaySamples >= irLength - 1 ||
                     (int)targetRightDelaySamples >= irLength - 1) continue;
 
-                float leftAmplitude = DistanceAmplitude * (1 - binauralFactor) * ray.Absorbtion;
-                float rightAmplitude = DistanceAmplitude * (1 + binauralFactor) * ray.Absorbtion;
+                float leftAmplitude = distanceAmplitude * (1 - binauralFactor) * ray.Absorbtion;
+                float rightAmplitude = distanceAmplitude * (1 + binauralFactor) * ray.Absorbtion;
 
                 _impulseResponseLeft[(int)targetLeftDelaySamples] += leftAmplitude;
                 _impulseResponseRight[(int)targetRightDelaySamples] += rightAmplitude;
@@ -132,14 +123,27 @@ namespace Code
                 _impulseResponseLeft[(int)targetLeftDelaySamples - 1] += leftAmplitude / 3;
                 _impulseResponseRight[(int)targetRightDelaySamples - 1] += rightAmplitude / 3;
             }
+
+            Task.Run(() => {_freqDomainIrLeft = reverb.ToFreqDomain(_impulseResponseLeft,_previousImpulseResponsesLeft); });
+            Task.Run(() => {_freqDomainIrRight = reverb.ToFreqDomain(_impulseResponseRight,_previousImpulseResponsesRight); });
+
+            if (_previousImpulseResponsesRight.Count > 5)
+            {
+                _previousImpulseResponsesRight.RemoveAt(0);
+                _previousImpulseResponsesLeft.RemoveAt(0); 
+            }
+            
+            
+            _previousImpulseResponsesLeft.Add(_impulseResponseLeft);
+            _previousImpulseResponsesRight.Add(_impulseResponseRight);
         }
 
 
         void OnAudioFilterRead(float[] data, int channels)
         {
             if (bypass || channels < 2 || !_isSetup) return;
-            if (_impulseResponseLeft == null || _impulseResponseRight == null) return;
-            
+            if (_freqDomainIrLeft == null || _freqDomainIrRight == null) return;
+
             float[] dataLeft = new float[data.Length / 2];
             float[] dataRight = new float[data.Length / 2];
 
@@ -151,114 +155,17 @@ namespace Code
             }
 
 
-            dataLeft = reverb.ConvolveData(_impulseResponseLeft, dataLeft,ref _overlapBufferLeft);
-            dataRight =reverb.ConvolveData(_impulseResponseRight, dataRight,ref _overlapBufferRight);
+            dataLeft = reverb.ConvolveData(_freqDomainIrLeft, dataLeft, ref _overlapBufferLeft);
+            dataRight = reverb.ConvolveData(_freqDomainIrRight, dataRight, ref _overlapBufferRight);
 
 
             for (int i = 0, j = 0; i < data.Length; i += 2, j++)
             {
                 data[i] = dataLeft[j];
                 data[i + 1] = dataRight[j];
-            } /*
-
-            List<AudioRay> rays = GetAllSelectedRays();
-
-            for (int i = 0; i < data.Length; i += 2)
-            {
-                float dry = (data[i] + data[i + 1]) * 0.5f;
-
-                // Pufferbeschreiben durch alle Rays
-                foreach (var ray in rays)
-                {
-                    if (!ray.IsValid) continue;
-
-                    float leftDistance = ray.DistanceToImage + Vector3.Distance(ray.ImagePosition, _leftEar);
-                    float rightDistance = ray.DistanceToImage + Vector3.Distance(ray.ImagePosition, _rightEar);
-
-                    float leftDelaySec = leftDistance / 343f;
-                    float rightDelaySec = rightDistance / 343f;
-
-                    float targetLeftDelaySamples = _sampleRate * leftDelaySec;
-                    float targetRightDelaySamples = _sampleRate * rightDelaySec;
-
-                    prevLeftDelaySamples = Mathf.Lerp(prevLeftDelaySamples, targetLeftDelaySamples, delaySmoothFactor);
-                    prevRightDelaySamples =
-                        Mathf.Lerp(prevRightDelaySamples, targetRightDelaySamples, delaySmoothFactor);
-
-                    int leftDelaySamples = Mathf.Clamp(Mathf.RoundToInt(prevLeftDelaySamples), 0, _bufferLength - 1);
-                    int rightDelaySamples = Mathf.Clamp(Mathf.RoundToInt(prevRightDelaySamples), 0, _bufferLength - 1);
-
-                    float maxEarDist = Vector3.Distance(_rightEar, _leftEar);
-                    float binauralFactor = Mathf.Clamp((leftDistance - rightDistance) / (4 * maxEarDist), -1f, 1f);
-
-                    int writeL = (_writeIndex + leftDelaySamples) % _bufferLength;
-                    int writeR = (_writeIndex + rightDelaySamples) % _bufferLength;
-
-
-                    float averageDistance = (leftDistance + rightDistance) / 2;
-
-                    float DistanceAmplitude = 2 / averageDistance;
-
-                    _delayBufferLeft[writeL] += Gain * DistanceAmplitude * dry * (1 - binauralFactor) * ray.Absorbtion;
-                    _delayBufferRight[writeR] += Gain * DistanceAmplitude * dry * (1 + binauralFactor) * ray.Absorbtion;
-
-                }
-
-                // Lese Puffer & gib binaurales Signal aus
-                float outL = _delayBufferLeft[_writeIndex];
-                float outR = _delayBufferRight[_writeIndex];
-
-                _delayBufferLeft[_writeIndex] = 0f;
-                _delayBufferRight[_writeIndex] = 0f;
-
-                data[i] = outL;
-                data[i + 1] = outR;
-
-                _writeIndex = (_writeIndex + 1) % _bufferLength;
-            }*/
+            } 
         }
-
-        private void ConvolveIr(float[] data, float[] ir, float[] buffer, int writeIndex)
-        {
-            int dataLen = data.Length;
-            int irLen = ir.Length;
-            int bufferLen = buffer.Length;
-            for (int n = 0; n < dataLen; ++n)
-            {
-                int idx = (writeIndex + n) % bufferLen;
-                buffer[idx] = 0f;
-            }
-
-            Parallel.For<float[]>(0, dataLen,
-                () => new float[bufferLen],
-                (i, loopState, localBuf) =>
-                {
-                    float sample = data[i];
-                    for (int j = 0; j < irLen; ++j)
-                    {
-                        int outIndex = (writeIndex + i + j) % bufferLen;
-                        localBuf[outIndex] += sample * ir[j];
-                    }
-
-                    return localBuf;
-                },
-                localBuf =>
-                {
-                    lock (buffer)
-                    {
-                        for (int k = 0; k < bufferLen; ++k)
-                            buffer[k] += localBuf[k];
-                    }
-                }
-            );
-        }
-
-
-        private int GetOverlapBufferIndex(int bufferLength, int accessIndex)
-        {
-            if (accessIndex >= 0) return accessIndex % bufferLength;
-            return bufferLength + accessIndex;
-        }
+        
 
         private List<AudioRay> GetAllSelectedRays()
         {
--- AudioProjectURP/Assets/Code/LiveConvolutionReverb.cs
+++ AudioProjectURP/Assets/Code/LiveConvolutionReverb.cs
@@ -1,114 +1,172 @@
+using System.Collections.Generic;
 using System.Numerics;
 using MathNet.Numerics.IntegralTransforms;
 using MathNet.Numerics.Providers.FourierTransform;
 using UnityEngine;
 
-public class LiveConvolutionReverb : MonoBehaviour
+namespace Code
 {
-    [Range(0, 1)] [SerializeField] public float wet;
-
-    private void Start()
+    public class LiveConvolutionReverb : MonoBehaviour
     {
-        FourierTransformControl.TryUseNativeMKL();
-    }
+        [Range(0, 1)] [SerializeField] public float wet;
 
-    private int GetMaxZweierPotenz(int size)
-    {
-        int fftLen = 1;
-        while (fftLen < size)
-            fftLen <<= 1;
-        return fftLen;
-    }
-
-    private float[] CutIR(int length , float[] ir)
-    {
-        float[] halfIr = new float[length];
-        for (int i = 0; i < halfIr.Length; i++)
+        private void Start()
         {
-            halfIr[i] = ir[i];
+            FourierTransformControl.TryUseNativeMKL();
         }
 
-        return halfIr;
-    }
-
-    public float[] ConvolveData(float[] ir, float[] audio,ref float[] overlapBuffer)
-    {
-        int lengthSum = audio.Length + ir.Length;
-        int requiredLength = GetMaxZweierPotenz(lengthSum);
-        if (overlapBuffer == null)
+        private int GetMaxZweierPotenz(int size)
         {
-            overlapBuffer = new float[requiredLength];
+            int fftLen = 1;
+            while (fftLen < size)
+                fftLen <<= 1;
+            return fftLen;
         }
 
-        Complex[] complexAudioInput = GetComplex(audio, requiredLength);
-
-        Fourier.Forward(complexAudioInput, FourierOptions.Matlab);
-        
-        Complex[] complexIrInput = GetComplex(ir, requiredLength);
-        Fourier.Forward(complexIrInput, FourierOptions.Matlab);
-        
-        for (int i = 0; i < complexIrInput.Length; i++)
+        private float[] CutIR(int length, float[] ir)
         {
-            complexAudioInput[i] *= complexIrInput[i];
+            float[] halfIr = new float[length];
+            for (int i = 0; i < halfIr.Length; i++)
+            {
+                halfIr[i] = ir[i];
+            }
+
+            return halfIr;
         }
 
-        Fourier.Inverse(complexAudioInput, FourierOptions.Matlab);
-
-        complexAudioInput = Normalize(complexAudioInput);
-        float[] resultingSamples = new float[complexAudioInput.Length];
-
-        overlapBuffer = PullOverlapBuffer(overlapBuffer, audio.Length);
-        for (int i = 0; i < resultingSamples.Length; i++)
+        public Complex[] ToFreqDomain(float[] inTimeDomain, List<float[]> previousInTimeDomain)
         {
-            overlapBuffer[i] += (float)complexAudioInput[i].Real;
+            for (int i = 0; i < previousInTimeDomain.Count; i++)
+            {
+                float[] prev = previousInTimeDomain[i];
+                for (int j = 0; j < inTimeDomain.Length; j++)
+                {
+                    inTimeDomain[j] += prev[j];
+                }
+            }
+
+            for (int i = 0; i < inTimeDomain.Length; i++)
+            {
+                inTimeDomain[i] /= previousInTimeDomain.Count*2;
+            }
+
+            int lengthSum = 1024 + inTimeDomain.Length;
+            int requiredLength = GetMaxZweierPotenz(lengthSum);
+            Complex[] complexIr = GetComplex(inTimeDomain, requiredLength);
+            Fourier.Forward(complexIr, FourierOptions.Matlab);
+            return complexIr;
         }
 
-        return Mix(audio, overlapBuffer);
-    }
-
-    private float[] PullOverlapBuffer(float[] overlapBuffer,int pullLength)
-    {
-        for (int i = 0; i < overlapBuffer.Length - pullLength; i++)
+        public float[] ConvolveData(float[] ir, float[] audio, ref float[] overlapBuffer)
         {
-            overlapBuffer[i] = overlapBuffer[i + pullLength];
+            int lengthSum = audio.Length + ir.Length;
+            int requiredLength = GetMaxZweierPotenz(lengthSum);
+            if (overlapBuffer == null)
+            {
+                overlapBuffer = new float[requiredLength];
+            }
+
+            Complex[] complexAudioInput = GetComplex(audio, requiredLength);
+
+            Fourier.Forward(complexAudioInput, FourierOptions.Matlab);
+
+            Complex[] complexIrInput = GetComplex(ir, requiredLength);
+            Fourier.Forward(complexIrInput, FourierOptions.Matlab);
+
+            for (int i = 0; i < complexIrInput.Length; i++)
+            {
+                complexAudioInput[i] *= complexIrInput[i];
+            }
+
+            Fourier.Inverse(complexAudioInput, FourierOptions.Matlab);
+
+            complexAudioInput = Normalize(complexAudioInput);
+            float[] resultingSamples = new float[complexAudioInput.Length];
+
+            overlapBuffer = PullOverlapBuffer(overlapBuffer, audio.Length);
+            for (int i = 0; i < resultingSamples.Length; i++)
+            {
+                overlapBuffer[i] += (float)complexAudioInput[i].Real;
+            }
+
+            return Mix(audio, overlapBuffer);
         }
 
-        for (int i = overlapBuffer.Length - pullLength; i < overlapBuffer.Length; i++)
+        public float[] ConvolveData(Complex[] irFreqDomain, float[] audio, ref float[] overlapBuffer)
         {
-            overlapBuffer[i] = 0;
+            int requiredLength = irFreqDomain.Length;
+            if (overlapBuffer == null)
+            {
+                overlapBuffer = new float[requiredLength];
+            }
+
+            Complex[] complexAudioInput = GetComplex(audio, requiredLength);
+
+            Fourier.Forward(complexAudioInput, FourierOptions.Matlab);
+
+            for (int i = 0; i < irFreqDomain.Length; i++)
+            {
+                complexAudioInput[i] *= irFreqDomain[i];
+            }
+
+            Fourier.Inverse(complexAudioInput, FourierOptions.Matlab);
+
+            complexAudioInput = Normalize(complexAudioInput);
+            float[] resultingSamples = new float[complexAudioInput.Length];
+
+            overlapBuffer = PullOverlapBuffer(overlapBuffer, audio.Length);
+            for (int i = 0; i < resultingSamples.Length; i++)
+            {
+                overlapBuffer[i] += (float)complexAudioInput[i].Real;
+            }
+
+            return Mix(audio, overlapBuffer);
         }
 
-        return overlapBuffer;
-    }
-
-    private float[] Mix(float[] dry, float[] wet)
-    {
-        float[] result = new float[dry.Length];
-        for (int i = 0; i < dry.Length; i++)
+        private float[] PullOverlapBuffer(float[] overlapBuffer, int pullLength)
         {
-            result[i] = dry[i] * (1 - this.wet)+ wet[i] * (this.wet);
+            for (int i = 0; i < overlapBuffer.Length - pullLength; i++)
+            {
+                overlapBuffer[i] = overlapBuffer[i + pullLength];
+            }
+
+            for (int i = overlapBuffer.Length - pullLength; i < overlapBuffer.Length; i++)
+            {
+                overlapBuffer[i] = 0;
+            }
+
+            return overlapBuffer;
         }
 
-        return result;
-    }
-
-    private Complex[] Normalize(Complex[] complexAudioInput)
-    {
-        for (int i = 0; i < complexAudioInput.Length; i++)
-            complexAudioInput[i] /= 5;
-
-        return complexAudioInput;
-    }
-
-    private Complex[] GetComplex(float[] buffer, int requiredLength)
-    {
-        Complex[] complexAudioData = new Complex[requiredLength];
-        for (int i = 0; i < buffer.Length; i++)
+        private float[] Mix(float[] dry, float[] wet)
         {
-            Complex tmp = new Complex(buffer[i], 0);
-            complexAudioData[i] = tmp;
+            float[] result = new float[dry.Length];
+            for (int i = 0; i < dry.Length; i++)
+            {
+                result[i] = dry[i] * (1 - this.wet) + wet[i] * (this.wet);
+            }
+
+            return result;
         }
 
-        return complexAudioData;
+        private Complex[] Normalize(Complex[] complexAudioInput)
+        {
+            for (int i = 0; i < complexAudioInput.Length; i++)
+                complexAudioInput[i] /= 5;
+
+            return complexAudioInput;
+        }
+
+        private Complex[] GetComplex(float[] buffer, int requiredLength)
+        {
+            Complex[] complexAudioData = new Complex[requiredLength];
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                Complex tmp = new Complex(buffer[i], 0);
+                complexAudioData[i] = tmp;
+            }
+
+            return complexAudioData;
+        }
     }
 }
