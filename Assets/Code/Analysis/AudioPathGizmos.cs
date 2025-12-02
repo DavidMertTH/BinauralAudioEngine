a9349a168113a6b479e4579e0253e3a6c7666c00
arthurkehrwald <50906979+arthurkehrwald@users.noreply.github.com>
2025-12-03 00:07:44 +0100

    Draw audio paths

---
+++ Assets/Code/Analysis/AudioPathGizmos.cs
@@ -0,0 +1,26 @@
+using Code.Core;
+using UnityEngine;
+
+namespace Code.Analysis
+{
+    /// <summary>
+    /// Traces audio paths in the Editor
+    /// </summary>
+    public class AudioPathGizmos : MonoBehaviour
+    {
+        private void Update()
+        {
+            foreach (var path in BinauralAudioEngine.Instance.AudioPaths)
+            {
+                if (!path.IsValid || path.Positions.Length < 2)
+                    continue;
+                var lineStartPos = path.Positions[0];
+                for (var i = 1; i < path.Positions.Length; i++)
+                {
+                    Debug.DrawLine(lineStartPos, path.Positions[i], Color.grey);
+                    lineStartPos = path.Positions[i];
+                }
+            }
+        }
+    }
+}
