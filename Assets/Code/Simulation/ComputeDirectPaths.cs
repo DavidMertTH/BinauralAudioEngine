e7ba90490d30126cefac01b9c7b9e0754ee3a221
arthurkehrwald <50906979+arthurkehrwald@users.noreply.github.com>
2026-02-25 19:49:04 +0100

    Fix direct paths

--- Assets/Code/Simulation/ComputeDirectPaths.cs
+++ Assets/Code/Simulation/ComputeDirectPaths.cs
@@ -3,9 +3,7 @@ using Unity.Burst;
 using Unity.Collections;
 using Unity.Jobs;
 using Unity.Mathematics;
-using static Unity.Mathematics.math;
 using UnityEngine;
-using Unity.Collections.LowLevel.Unsafe;
 
 namespace Code.Simulation
 {
@@ -14,97 +12,40 @@ namespace Code.Simulation
     /// </summary>
     public class ComputeDirectPaths : IDisposable
     {
-        private NativeArray<RaycastCommand> _commands;
-        private NativeArray<RaycastHit> _hits;
-
         public JobHandle Schedule(float3 listener, NativeArray<float3>.ReadOnly sources, LayerMask rayMask,
             NativeArray<AudioPath> paths)
         {
-            // Create raycast commands
-            _commands = Helper.ReallocateIfNeeded(_commands, sources.Length, Allocator.Persistent);
-            var createCommands = new CreateDirectRaycastCommands()
+            // This runs synchronously on the main thread because RaycastCommand.ScheduleBatch did not work correctly,
+            // and I could not figure out why.
+            for (var i = 0; i < sources.Length; i++)
             {
-                Commands = _commands,
-                Sources = sources,
-                Listener = listener,
-                RayMask = rayMask
-            };
-            var createCommandsHandle = createCommands.ScheduleParallel(_commands.Length, 32, default);
-
-            // Perform raycasts
-            _hits = Helper.ReallocateIfNeeded(_hits, sources.Length, Allocator.Persistent);
-            var execRaycastsHandle = RaycastCommand.ScheduleBatch(_commands, _hits, 1, dependsOn: createCommandsHandle);
-
-            // Evaluate raycasts -> audio paths
-            var evaluateRaycastsJob = new EvaluateRaycasts()
-            {
-                Paths = paths,
-                Hits = _hits,
-                Commands = _commands,
-                Listener = listener,
-                Sources = sources,
-            };
-            var evaluateRaycastsHandle =
-                evaluateRaycastsJob.ScheduleParallel(sources.Length, 32, dependency: execRaycastsHandle);
-            return evaluateRaycastsHandle;
-        }
-
-        [BurstCompile]
-        private struct CreateDirectRaycastCommands : IJobFor
-        {
-            public NativeArray<RaycastCommand> Commands;
-
-            [ReadOnly] public NativeArray<float3>.ReadOnly Sources;
-            [ReadOnly] public float3 Listener;
-            [ReadOnly] public LayerMask RayMask;
-
-            public void Execute(int index)
-            {
-                var direction = Sources[index] - Listener;
-                var distance = length(direction);
-                Commands[index] = new RaycastCommand(Listener, direction, new QueryParameters(RayMask), distance);
-            }
-        }
-
-        /// <summary>
-        /// Create audio paths from the raycast results.
-        /// </summary>
-        [BurstCompile]
-        private struct EvaluateRaycasts : IJobFor
-        {
-            // Disable safety checks to write to the <c>Paths</c> sub-array
-            // while other jobs are writing to different sub-arrays of the same array.
-            [NativeDisableContainerSafetyRestriction]
-            public NativeArray<AudioPath> Paths;
-
-            [ReadOnly] public NativeArray<RaycastHit> Hits;
-            [ReadOnly] public NativeArray<RaycastCommand> Commands;
-            [ReadOnly] public NativeArray<float3>.ReadOnly Sources;
-            [ReadOnly] public float3 Listener;
-
-            public void Execute(int index)
-            {
-                var path = Paths[index];
-                path.IsValid = !Helper.DidHit(Hits[index]);
-                if (path.IsValid)
+                var dist = math.distance(listener, sources[i]);
+                var hit = Physics.Raycast(sources[i], listener - sources[i], dist, rayMask);
+                if (hit)
                 {
-                    path.SourceIndex = index;
-                    path.Reflections = 0;
-                    path.DistanceToImage = Commands[index].distance;
-                    path.Energy = 1f;
-                    path.Positions.Clear();
-                    path.Positions.Add(Listener);
-                    path.Positions.Add(Sources[index]);
+                    paths[i] = new AudioPath() { IsValid = false };
+                    continue;
                 }
-
-                Paths[index] = path;
+                
+                var path = new AudioPath()
+                {
+                    IsValid = true,
+                    DistanceToImage = dist,
+                    Energy = 1f,
+                    ImagePosition = sources[i],
+                    SourceIndex = i,
+                };
+                path.Positions.Clear();
+                path.Positions.Add(listener);
+                path.Positions.Add(sources[i]);
+                paths[i] = path;
             }
+            
+            return new JobHandle();
         }
 
         public void Dispose()
         {
-            _commands.Dispose();
-            _hits.Dispose();
         }
     }
 }
